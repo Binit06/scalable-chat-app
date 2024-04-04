593463411d9241c266be08449023460d498f80a8
Binit <binitlenka@gmail.com>
2024-04-04 18:31:39 +0530

    Fixed: Remove dotenv

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -12,7 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
 const http_1 = __importDefault(require("http"));
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
